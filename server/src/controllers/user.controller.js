6f382b824bd68b14ea82baf49d7d9ecb2e846d62
akhilesh-code-96 <srivastavakhilesh157@gmail.com>
2024-07-16 19:06:37 +0530

    ..

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -46,13 +46,11 @@ export default class UserController {
         return res.status(401).json({ error: "Invalid email or password" });
       }
       req.session.userEmail = email;
-      res
-        .status(200)
-        .json({
-          user,
-          sessionId: req.session.id,
-          email: req.session.userEmail,
-        });
+      res.status(200).json({
+        user,
+        sessionId: req.session.id,
+        email: req.session.userEmail,
+      });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -71,10 +69,10 @@ export default class UserController {
   async updateAbout(req, res) {
     const { email, about } = req.body;
     try {
-      await UserModel.updateOne({email: email}, {$set: {about: about}});
-      res.status(200).json({message: "User about updated successfully"});
+      await UserModel.updateOne({ email: email }, { $set: { about: about } });
+      res.status(200).json({ message: "User about updated successfully" });
     } catch (error) {
-      res.status(501).json({message: "Failed to update the about"});
+      res.status(501).json({ message: "Failed to update the about" });
     }
   }
 
@@ -88,16 +86,15 @@ export default class UserController {
     });
   }
 
-
   async userInfo(req, res) {
-    const {userId} = req.body;
+    const { userId } = req.body;
     if (userId) {
       try {
-        const user = await UserModel.find({_id: userId});
+        const user = await UserModel.find({ _id: userId });
         // console.log(user);
         res.status(200).json({ user });
       } catch (error) {
-        res.status(404).json({message: "Failed to fetch the user"});
+        res.status(404).json({ message: "Failed to fetch the user" });
       }
     }
   }
